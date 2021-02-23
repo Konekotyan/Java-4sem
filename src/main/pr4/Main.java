3c14939108e43265e25c34342bf33e17111d59ea
Konekotyan <Pistolero01@mail.ru>
2021-02-23 16:57:08 +0300

    4th

---
+++ src/main/pr4/Main.java
@@ -0,0 +1,15 @@
+package main.pr4;
+
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Callable<String> task = () -> Thread.currentThread().getName();
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 5; i++) {
+            Future result = service.submit(task);
+            System.out.println(result.get());
+        }
+        service.shutdown();
+    }
+}
